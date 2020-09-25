217d02f5e04f7ccc22b3700238d78820f475bb6a
Junior Fernandes <junior.fernandes@explicae.com.br>
2020-09-25 19:46:20 -0300

    Enviando finalização da aula01

---
+++ TransicaoTela/TransicaoTela/Views/FirstVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransicaoTela
+//
+//  Created by Junior Fernandes on 23/09/20.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
